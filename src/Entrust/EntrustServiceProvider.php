72b55d8532b4a72064c79bb16d8dda20b874d9dd
Dmitry Borzyonok <dima.matters@gmail.com>
2016-01-20 15:08:45 +0200

    Return missed blade directives

--- src/Entrust/EntrustServiceProvider.php
+++ src/Entrust/EntrustServiceProvider.php
@@ -34,6 +34,9 @@ class EntrustServiceProvider extends ServiceProvider
         // Register commands
         $this->commands('command.entrust.migration');
         $this->commands('command.entrust.classes');
+
+        // Register blade directives
+        $this->bladeDirectives();
     }
 
     /**
@@ -48,6 +51,41 @@ class EntrustServiceProvider extends ServiceProvider
         $this->registerCommands();
 
         $this->mergeConfig();
+    }
+
+    /**
+     * Register the blade directives
+     *
+     * @return void
+     */
+    private function bladeDirectives()
+    {
+        // Call to Entrust::hasRole
+        \Blade::directive('role', function($expression) {
+            return "<?php if (\\Entrust::hasRole{$expression}) : ?>";
+        });
+
+        \Blade::directive('endrole', function($expression) {
+            return "<?php endif; // Entrust::hasRole ?>";
+        });
+
+        // Call to Entrust::can
+        \Blade::directive('permission', function($expression) {
+            return "<?php if (\\Entrust::can{$expression}) : ?>";
+        });
+
+        \Blade::directive('endpermission', function($expression) {
+            return "<?php endif; // Entrust::can ?>";
+        });
+
+        // Call to Entrust::ability
+        \Blade::directive('ability', function($expression) {
+            return "<?php if (\\Entrust::ability{$expression}) : ?>";
+        });
+
+        \Blade::directive('endability', function($expression) {
+            return "<?php endif; // Entrust::ability ?>";
+        });
     }
 
     /**
@@ -69,10 +107,10 @@ class EntrustServiceProvider extends ServiceProvider
      */
     private function registerCommands()
     {
-        $this->app->bindShared('command.entrust.migration', function ($app) {
+        $this->app->singleton('command.entrust.migration', function ($app) {
             return new MigrationCommand();
         });
-        $this->app->bindShared('command.entrust.classes', function ($app) {
+        $this->app->singleton('command.entrust.classes', function ($app) {
             return new ClassCreatorCommand();
         });
     }
